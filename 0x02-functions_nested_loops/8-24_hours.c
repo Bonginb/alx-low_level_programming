841dc63727d24c7480f006ae1c3eaf77e581eb50
Bonginb <bonginb.ramoba@gmail.com>
2023-02-22 13:37:05 -0800

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * jack_bauer -  function that prints every minute of the day 
+ * jack_bauer - function that prints every minute of the day 
  *
- * Return: no return
+ * Return: 0
  */
 
 void jack_bauer(void)
