5522b1378646fd91dc55bebfdb984f6affc33100
Bonginb <bonginb.ramoba@gmail.com>
2023-02-27 12:04:42 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,13 +9,14 @@
 int main(void)
 {
 	int x;
+
 	for (x = 1; x <= 100; x++)
 	{
 		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
-		else  if (x % 3 == 0)
+		else if (x % 3 == 0)
 		{
 			printf("Fizz");
 		}
@@ -23,7 +24,7 @@ int main(void)
 		{
 			printf("Buzz");
 		}
-		else 
+		else
 		{
 			printf("%d", x);
 		}
