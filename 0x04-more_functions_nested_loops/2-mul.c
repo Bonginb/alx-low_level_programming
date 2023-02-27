afeb8da9199b5dda1ed1e87178b4e356a613f273
Bonginb <bonginb.ramoba@gmail.com>
2023-02-27 09:21:35 -0800

    Multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * nul - Multiplies two integers
+ * mul - Multiplies two integers
  * @x: The first number to multiply
  * @y: The second number to multiply the first number
  * Return: The result of multiplication between x and y paran
